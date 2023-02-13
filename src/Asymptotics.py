02b9b1008f83f9ff243a8ca430fe86bb6e4dfa4e
vallerrr <valler4044@gmail.com>
2023-02-13 15:57:30 +0000

    find the raw data labels in dta_label_reader.ipynb

--- src/Asymptotics.py
+++ src/Asymptotics.py
@@ -6,21 +6,22 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from pathlib import Path
 
 def asymptotics(model_set, start, end, step, domains):
     Eva_fixed_test_size = pd.DataFrame(
-        columns=['model', 'train_subset_size', 'train_auc_score', 'test_auc_score', 'train_f1_score', 'test_f1_score','test_pr_auc','test_pr_no_skill',
+        columns=['model', 'train_subset_size', 'train_auc_score', 'test_auc_score', 'train_f1_score', 'test_f1_score',
+                 'test_pr_auc', 'test_pr_no_skill',
                  'train_efron_r2', 'test_efron_r2',
                  'train_ffc_r2', 'test_ffc_r2',
-                  'test_briern_r2',  'test_imv_r2'])
+                 'test_briern_r2', 'test_imv_r2'])
 
     for model in model_set:
         print('Model: {}'.format(model))
         for train_subset_size in np.arange(start, end, step):
             print('train set size = {}'.format(train_subset_size))
-            model_ = Models.Model_fixed_test_size(df, 0.3, domains, model, train_subset_size, polynomial,y_colname='death')
-            eva=Evaluate.metric(model_)
+            model_ = Models.Model_fixed_test_size(df, 0.3, domains, model, train_subset_size, polynomial,
+                                                  y_colname='death')
+            eva = Evaluate.metric(model_)
             Eva_fixed_test_size.loc[len(Eva_fixed_test_size)] = [model,
                                                                  len(model_.X_train),
 
@@ -52,6 +53,7 @@ def asymptotics(model_set, start, end, step, domains):
 
     return Eva_fixed_test_size
 
+
 random.seed(52194)
 pd.set_option('precision', 9)
 
@@ -67,58 +69,53 @@ domain_dict.keys()
 
 model_set = ['lgb']  # 'xgb', 'logreg']
 
-temp=np.arange(0.3,1.005,0.005)
+temp = np.arange(0.3, 1.005, 0.005)
 Eva_fixed_test_size = asymptotics(model_set=model_set, start=0.3, end=1.005, step=0.005, domains=domains)
 # Eva_fixed_test_size_new = asymptotics(model_set=model_set, start=0.9, end=1, step=0.005, domains=domains)
 # auc score
 # Eva_fixed_test_size.to_csv(Path.cwd()/'result_csv/asymptotics_lgb.csv')
 
 # Eva_fixed_test_size=pd.read_csv(Path.cwd()/'result_csv/asymptotics_lgb.csv',index_col=0)
-columns =[]
+columns = []
 for column in Eva_fixed_test_size.columns:
     if 'test' in column:
         columns.append(column)
 
-
-df_to_plot=Eva_fixed_test_size.copy()
-
+df_to_plot = Eva_fixed_test_size.copy()
 
 columns.remove('test_f1_score')
 columns.remove('test_pr_no_skill')
 
 ploted_col_dict = {'test_auc_score': 'ROC-AUC Score',
- # 'test_f1_score':'F1 Score',
+                   'test_efron_r2': 'Efron R2',
+                   'test_pr_auc': 'PR-AUC Score',
+                   'test_ffc_r2': 'FFC R2',
+                   'test_briern_r2': 'Brier Score',
+                   'test_imv_r2': 'IMV'}
 
- 'test_efron_r2':'Efron R2',
- 'test_pr_auc':'PR-AUC Score',
- 'test_ffc_r2':'FFC R2',
- 'test_briern_r2':'Brier Score','test_imv_r2': 'IMV'}
-
-color_yellow='#F1A52C'
-color_blue='#001C5B'
-
+color_yellow = '#F1A52C'
+color_blue = '#001C5B'
 
 import seaborn
-fig, axis = plt.subplots(2,3)
+
+fig, axis = plt.subplots(2, 3)
 fontsize_ticks = 14
 fontsize_labels = 15
-fig.subplots_adjust(left=0.08, bottom=0.08,top=0.95,right=0.99)
-plt.rcParams['figure.figsize']=[14,10]
+fig.subplots_adjust(left=0.08, bottom=0.08, top=0.95, right=0.99)
+plt.rcParams['figure.figsize'] = [14, 10]
 
-count=0
+count = 0
 for (m, n), subplot in np.ndenumerate(axis):
-    seaborn.regplot(x=df_to_plot['train_subset_size'].astype(float),y=df_to_plot[columns[count]].astype(float),color=color_blue,ax=axis[m, n])
-    axis[m, n].set_ylabel(ploted_col_dict[columns[count]],size=fontsize_labels)
-    axis[m,n].set_xlabel('Training Set Size',size=fontsize_labels)
+    seaborn.regplot(x=df_to_plot['train_subset_size'].astype(float), y=df_to_plot[columns[count]].astype(float),
+                    color=color_blue, ax=axis[m, n])
+    axis[m, n].set_ylabel(ploted_col_dict[columns[count]], size=fontsize_labels)
+    axis[m, n].set_xlabel('Training Set Size', size=fontsize_labels)
 
     axis[m, n].tick_params(axis='both', which='major', labelsize=fontsize_ticks)
     axis[m, n].spines['top'].set_visible(False)
     axis[m, n].spines['right'].set_visible(False)
-    axis[m,n].grid(axis='both',alpha=0.4)
+    axis[m, n].grid(axis='both', alpha=0.4)
     axis[m, n].set_axisbelow(True)
-    count+=1
+    count += 1
 plt.show()
 # plt.savefig(Path.cwd()/'graphs/asymptotics.pdf')
-
-
-
